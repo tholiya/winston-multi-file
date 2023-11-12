81c4ed9a299cf029f47fa4da5a481834c271049e
Rakesh Tholiya <rakesh16.t@gmail.com>
2023-11-12 16:42:34 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Winston Multi File Logger
+
+**winston-multi-file** is used to create multiple files based on label, base of this package is [winston](https://www.npmjs.com/package/winston), you can integrate other winston transporter also.
+
+
+# Install
+
+    npm i winston-multi-file
+
+## Use
+
+    import  winston  from  "winston"; //this is required for transporter
+	import  Logger  from  "winston-multi-file";
+
+	const  logger  =  new  Logger();
+	
+	//set log directory default will be logs
+	logger.setLogDir('logs'); 
+
+	//set custom format default format will be `${timestamp} : ${level} : ${message}`
+	logger.setFormat(({ level, message, label, timestamp }) => {
+		return  `${timestamp} : ${label} : ${level} : ${message}`;
+	});
+
+	//include console transporter, it will console each log
+	logger.transporter([new  winston.transports.Console({})])
+	//include file transporter, it will combined all logs in combined.log file
+	logger.transporter([new  winston.transports.File({ filename:  'combined.log' })])
+
+	const  userModuleLog  =  logger.getLogger('user_module');
+	const  paymentModuleLog  =  logger.getLogger('payment_module');
+
+	//creates user_module_YYYY_MM_DD.log file and add this log
+	userModuleLog.info('User has been created successfully.'); 
+	//creates payment_module_YYYY_MM_DD.log file and add this log
+	paymentModuleLog.error('payment of user failed.'); 
+
---
+++ index.js
@@ -0,0 +1,67 @@
+import winston from "winston";
+import 'winston-daily-rotate-file';
+
+class Logger {
+  // Private properties
+  #transporters = [];
+  #myFormat = winston.format.printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} : ${level} : ${message}`;
+  });
+  #logDir = 'logs';
+
+  // Setter for log directory
+  setLogDir(dir) {
+    this.#logDir = dir;
+  }
+
+  // Setter for log format
+  setFormat(format) {
+    this.#myFormat = winston.format.printf(format);
+  }
+
+  // Method to add custom transporters
+  transporter(transporters) {
+    for (const transporter of transporters) {
+      // If the transporter doesn't have a format, assign the default format
+      if (!transporter.format) {
+        transporter.format = this.#format();
+      }
+    }
+    // Concatenate the new transporters with existing ones
+    this.#transporters = [...this.#transporters, ...transporters];
+  }
+
+  // Method to get or create a logger
+  getLogger(label) {
+    if (!winston.loggers.has(label)) {
+      // Add a file transport specific to the provided label
+      this.#transporters.push(this.#fileTransport(label));
+      // Create or get the logger with specified transports and label format
+      winston.loggers.add(label, {
+        transports: this.#transporters,
+        format: winston.format.label({ label }),
+      });
+    }
+    // Return the logger with the specified label
+    return winston.loggers.get(label);
+  }
+
+  // Private method to define the log format
+  #format() {
+    return winston.format.combine(
+      winston.format.timestamp(),
+      this.#myFormat,
+    );
+  }
+
+  // Private method to configure the file transport
+  #fileTransport = (label) => {
+    return new winston.transports.DailyRotateFile({
+      format: this.#format(),
+      filename: `${this.#logDir}/${label}_%DATE%.log`,
+      datePattern: 'YYYY_MM_DD',
+    });
+  };
+}
+
+export default Logger;
